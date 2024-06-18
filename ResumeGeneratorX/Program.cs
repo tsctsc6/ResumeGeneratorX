17754c1f0fc49d0306970a51a882588e140c9f70
tsctsc6@126.com <tsctsc6@126.com>
2024-06-18 15:17:38 +0800

    添加项目文件。

---
+++ ResumeGeneratorX/Program.cs
@@ -0,0 +1,55 @@
+﻿using System.CommandLine;
+
+namespace ResumeGeneratorX
+{
+    internal class Program
+    {
+        static async Task<int> Main(string[] args)
+        {
+            var inputFileOption = new Option<FileInfo?>(
+                name: "--input-file",
+                description: "Input file")
+            { IsRequired = true };
+            inputFileOption.AddAlias("-i");
+            inputFileOption.Arity = ArgumentArity.ExactlyOne;
+
+            var outputDirectoryOption = new Option<DirectoryInfo?>(
+                name: "--output-directory",
+                description: "Output directory");
+            outputDirectoryOption.AddAlias("-o");
+            outputDirectoryOption.Arity = ArgumentArity.ZeroOrOne;
+
+            var templateOption = new Option<int?>(
+                name: "--template",
+                description: "Resume template")
+                .FromAmong("1", "2", "3");
+            templateOption.AddAlias("-t");
+            templateOption.Arity = ArgumentArity.ZeroOrOne;
+
+            var rootCommand = new RootCommand("Generate Resume HTML from JSON.");
+            rootCommand.AddOption(inputFileOption);
+            rootCommand.AddOption(outputDirectoryOption);
+            rootCommand.AddOption(templateOption);
+
+            rootCommand.SetHandler((input, output, template) =>
+            {
+                if (input is null) return;
+                if (!File.Exists(input.FullName))
+                {
+                    Console.Error.WriteLine($"Input file \"{input.FullName}\" does not exist.");
+                    return;
+                }
+                Main2(input, output ?? input.Directory!, template ?? 2);
+            }, inputFileOption, outputDirectoryOption, templateOption);
+
+            return await rootCommand.InvokeAsync(args);
+        }
+
+        static void Main2(FileInfo input, DirectoryInfo output, int template)
+        {
+            Console.WriteLine(input);
+            Console.WriteLine(output);
+            Console.WriteLine(template);
+        }
+    }
+}
---
+++ ResumeGeneratorX/ResumeInfo.cs
@@ -0,0 +1,107 @@
+﻿namespace ResumeGeneratorX
+{
+
+    public class ResumeInfo
+    {
+        public TitleNameMap TitleNameMap { get; set; } = new();
+        public Avatar Avatar { get; set; } = new();
+        public Profile Profile { get; set; } = new();
+        public Education[]? EducationList { get; set; }
+        public Award[]? AwardList { get; set; }
+        public WorkExp[]? WorkExpList { get; set; }
+        public Skill[]? SkillList { get; set; }
+        public ProjectList[]? ProjectList { get; set; }
+        public Work[]? WorkList { get; set; }
+        public AboutMe? AboutMe { get; set; }
+        public Theme Theme { get; set; } = new();
+    }
+
+    public class TitleNameMap
+    {
+        public string EducationList { get; set; } = "教育背景";
+        public string WorkExpList { get; set; } = "工作经历";
+        public string ProjectList { get; set; } = "项目经历";
+        public string SkillList { get; set; } = "个人技能";
+        public string AwardList { get; set; } = "更多信息";
+        public string WorkList { get; set; } = "个人作品";
+        public string AboutMe { get; set; } = "自我介绍";
+    }
+
+    public class Avatar
+    {
+        public string Src { get; set; } = "./DefaultAvatar.png";
+        public bool IsHidden { get; set; } = false;
+        public string Shape { get; set; } = "square";
+    }
+
+    public class Profile
+    {
+        public string? Name { get; set; }
+        public string? Email { get; set; }
+        public string? Mobile { get; set; }
+        public string? Github { get; set; }
+        public string? Zhihu { get; set; }
+        public string? WorkExpYear { get; set; }
+        public string? WorkPlace { get; set; }
+        public string? PositionTitle { get; set; }
+    }
+
+    public class AboutMe
+    {
+        public string? AboutMe_Desc { get; set; }
+    }
+
+    public class Theme
+    {
+        public string Color { get; set; } = "#2f5785";
+        public string TagColor { get; set; } = "#8bc34a";
+    }
+
+    public class Education
+    {
+        public string? BeginTime { get; set; }
+        public string? EndTime { get; set; }
+        public string? School { get; set; }
+        public string? Major { get; set; }
+        public string? AcademicDegree { get; set; }
+    }
+
+    public class Award
+    {
+        public string? AwardInfo { get; set; }
+        public string? AwardTime { get; set; }
+    }
+
+    public class WorkExp
+    {
+        public string? CompanyName { get; set; }
+        public string? DepartmentName { get; set; }
+        public string? BeginTime { get; set; }
+        public string? EndTime { get; set; }
+        public string? WorkDesc { get; set; }
+    }
+
+    public class Skill
+    {
+        public string? skillName { get; set; }
+        public string? SkillDesc { get; set; }
+        public int SkillLevel { get; set; }
+    }
+
+    public class ProjectList
+    {
+        public string? ProjectName { get; set; }
+        public string? ProjectRole { get; set; }
+        public string? ProjectTime { get; set; }
+        public string? ProjectDesc { get; set; }
+        public string? ProjectContent { get; set; }
+    }
+
+    public class Work
+    {
+        public string? WorkName { get; set; }
+        public string? WorkDesc { get; set; }
+        public string? VisitLink { get; set; }
+    }
+
+}
