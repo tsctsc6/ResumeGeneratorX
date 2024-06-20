fe91555f49e3e38f0c4c4369db1a8337b1048ce2
tsctsc6@126.com <tsctsc6@126.com>
2024-06-20 21:58:31 +0800

    润色代码

--- ResumeGeneratorX/Template2Gen.cs
+++ ResumeGeneratorX/Template2Gen.cs
@@ -79,7 +79,8 @@ namespace ResumeGeneratorX
 
         private void GenSectionProjecTitle(StringBuilder sb)
         {
-            sb.Append("<div class=\"section-title\" style=\"color: rgb(47, 87, 133);\"><span class=\"title\">");
+            sb.Append("<div class=\"section-title\" style=\"color: rgb(47, 87, 133);\">");
+            sb.Append("<span class=\"title\">");
             sb.Append(rio.TitleNameMap.ProjectList);
             sb.Append("</span><span class=\"title-addon\"></span></div>");
         }
@@ -94,7 +95,8 @@ namespace ResumeGeneratorX
 
         private void GenSectionExperienceBody(StringBuilder sb)
         {
-            sb.Append("<div class=\"section-body\"><div class=\"section section-work-exp\">");
+            sb.Append("<div class=\"section-body\">");
+            sb.Append("<div class=\"section section-work-exp\">");
             if (rio.WorkExpList is not null)
                 foreach (var item in rio.WorkExpList)
                     GeGenSectionExperienceItem(sb, item);
@@ -202,7 +204,8 @@ namespace ResumeGeneratorX
 
         private void GenSectionSkillTitle(StringBuilder sb)
         {
-            sb.Append("<div class=\"section-title\" style=\"color: rgb(47, 87, 133);\"><span class=\"title\">");
+            sb.Append("<div class=\"section-title\" style=\"color: rgb(47, 87, 133);\">");
+            sb.Append("<span class=\"title\">");
             sb.Append(rio.TitleNameMap.SkillList);
             sb.Append("</span><span class=\"title-addon\"></span></div>");
         }
@@ -210,7 +213,8 @@ namespace ResumeGeneratorX
         private void GenSectionAboutMe(StringBuilder sb)
         {
             if (rio.AboutMe == null) return;
-            sb.Append("<div class=\"section-title\" style=\"color: rgb(47, 87, 133);\"><span class=\"title\">");
+            sb.Append("<div class=\"section-title\" style=\"color: rgb(47, 87, 133);\">");
+            sb.Append("<span class=\"title\">");
             sb.Append(rio.TitleNameMap.AboutMe);
             sb.Append("</span><span class=\"title-addon\"></span></div>");
             sb.Append("<div class=\"section-body\">");
@@ -253,7 +257,8 @@ namespace ResumeGeneratorX
 
         private void GenSectionWorkTitle(StringBuilder sb)
         {
-            sb.Append("<div class=\"section-title\" style=\"color: rgb(47, 87, 133);\"><span class=\"title\">");
+            sb.Append("<div class=\"section-title\" style=\"color: rgb(47, 87, 133);\">");
+            sb.Append("<span class=\"title\">");
             sb.Append(rio.TitleNameMap.WorkList);
             sb.Append("</span><span class=\"title-addon\"></span></div>");
         }
@@ -291,7 +296,8 @@ namespace ResumeGeneratorX
 
         private void GenSectionEducationTitle(StringBuilder sb)
         {
-            sb.Append("<div class=\"section-title\" style=\"color: rgb(47, 87, 133);\"><span class=\"title\">");
+            sb.Append("<div class=\"section-title\" style=\"color: rgb(47, 87, 133);\">");
+            sb.Append("<span class=\"title\">");
             sb.Append(rio.TitleNameMap.EducationList);
             sb.Append("</span><span class=\"title-addon\"></span></div>");
         }
@@ -306,7 +312,8 @@ namespace ResumeGeneratorX
 
         private void GenAvatar(StringBuilder sb)
         {
-            sb.Append("<div class=\"avatar \"><span class=\"ant-avatar ant-avatar-square ant-avatar-image avatar\">");
+            sb.Append("<div class=\"avatar \">");
+            sb.Append("<span class=\"ant-avatar ant-avatar-square ant-avatar-image avatar\">");
             sb.Append($"<img src=\"{rio.Avatar.Src}\">");
             sb.Append("</span></div>");
         }
@@ -340,44 +347,54 @@ namespace ResumeGeneratorX
 
         private void GenExpectJob(StringBuilder sb)
         {
-            sb.Append("<div class=\"expect-job\"><span role=\"img\" aria-label=\"heart\" class=\"anticon anticon-heart\" style=\"color: rgb(47, 87, 133); opacity: 0.85;\"><svg viewBox=\"64 64 896 896\" focusable=\"false\" data-icon=\"heart\" width=\"1em\" height=\"1em\" fill=\"currentColor\" aria-hidden=\"true\">");
+            sb.Append("<div class=\"expect-job\">");
+            sb.Append("<span role=\"img\" aria-label=\"heart\" class=\"anticon anticon-heart\" style=\"color: rgb(47, 87, 133); opacity: 0.85;\">");
+            sb.Append("<svg viewBox=\"64 64 896 896\" focusable=\"false\" data-icon=\"heart\" width=\"1em\" height=\"1em\" fill=\"currentColor\" aria-hidden=\"true\">");
             sb.Append(File.ReadAllText($"{assetsBasePath}\\svg.path\\heart.txt"));
             sb.Append($"</svg></span><span>{rio.Profile.PositionTitle}</span></div>");
         }
 
         private void GenWorkPlace(StringBuilder sb)
         {
-            sb.Append("<div class=\"work-place\"><span role=\"img\" aria-label=\"environment\" class=\"anticon anticon-environment\" style=\"color: rgb(47, 87, 133); opacity: 0.85;\"><svg viewBox=\"64 64 896 896\" focusable=\"false\" data-icon=\"environment\" width=\"1em\" height=\"1em\" fill=\"currentColor\" aria-hidden=\"true\">");
+            sb.Append("<div class=\"work-place\">");
+            sb.Append("<span role=\"img\" aria-label=\"environment\" class=\"anticon anticon-environment\" style=\"color: rgb(47, 87, 133); opacity: 0.85;\">");
+            sb.Append("<svg viewBox=\"64 64 896 896\" focusable=\"false\" data-icon=\"environment\" width=\"1em\" height=\"1em\" fill=\"currentColor\" aria-hidden=\"true\">");
             sb.Append(File.ReadAllText($"{assetsBasePath}\\svg.path\\environment.txt"));
             sb.Append($"</svg></span><span>{rio.Profile.WorkPlace}</span></div>");
         }
 
         private void GenZhihu(StringBuilder sb)
         {
-            sb.Append("<div class=\"zhihu\"><span role=\"img\" aria-label=\"zhihu-circle\" class=\"anticon anticon-zhihu-circle\" style=\"color: rgb(47, 87, 133); opacity: 0.85;\"><svg viewBox=\"64 64 896 896\" focusable=\"false\" data-icon=\"zhihu-circle\" width=\"1em\" height=\"1em\" fill=\"currentColor\" aria-hidden=\"true\">");
+            sb.Append("<div class=\"zhihu\">");
+            sb.Append("<span role=\"img\" aria-label=\"zhihu-circle\" class=\"anticon anticon-zhihu-circle\" style=\"color: rgb(47, 87, 133); opacity: 0.85;\">");
+            sb.Append("<svg viewBox=\"64 64 896 896\" focusable=\"false\" data-icon=\"zhihu-circle\" width=\"1em\" height=\"1em\" fill=\"currentColor\" aria-hidden=\"true\">");
             sb.Append(File.ReadAllText($"{assetsBasePath}\\svg.path\\zhihu-circle.txt"));
             sb.Append($"</svg></span><span style=\"cursor: pointer;\">{rio.Profile.Zhihu}</span></div>");
         }
 
         private void GenGithub(StringBuilder sb)
         {
-            sb.Append("<div class=\"github\"><span role=\"img\" aria-label=\"github\" class=\"anticon anticon-github\" style=\"color: rgb(47, 87, 133); opacity: 0.85;\"><svg viewBox=\"64 64 896 896\" focusable=\"false\" data-icon=\"github\" width=\"1em\" height=\"1em\" fill=\"currentColor\" aria-hidden=\"true\">");
+            sb.Append("<div class=\"github\">");
+            sb.Append("<span role=\"img\" aria-label=\"github\" class=\"anticon anticon-github\" style=\"color: rgb(47, 87, 133); opacity: 0.85;\">");
+            sb.Append("<svg viewBox=\"64 64 896 896\" focusable=\"false\" data-icon=\"github\" width=\"1em\" height=\"1em\" fill=\"currentColor\" aria-hidden=\"true\">");
             sb.Append(File.ReadAllText($"{assetsBasePath}\\svg.path\\github.txt"));
             sb.Append($"</svg></span><span style=\"cursor: pointer;\">{rio.Profile.Github}</span></div>");
         }
 
         private void GenEmail(StringBuilder sb)
         {
-            sb.Append("<div class=\"email\"><span role=\"img\" aria-label=\"mail\" class=\"anticon anticon-mail\" style=\"color: rgb(47, 87, 133); opacity: 0.85;\"><svg viewBox=\"64 64 896 896\" focusable=\"false\" data-icon=\"mail\" width=\"1em\" height=\"1em\" fill=\"currentColor\" aria-hidden=\"true\">");
+            sb.Append("<div class=\"email\">");
+            sb.Append("<span role=\"img\" aria-label=\"mail\" class=\"anticon anticon-mail\" style=\"color: rgb(47, 87, 133); opacity: 0.85;\">");
+            sb.Append("<svg viewBox=\"64 64 896 896\" focusable=\"false\" data-icon=\"mail\" width=\"1em\" height=\"1em\" fill=\"currentColor\" aria-hidden=\"true\">");
             sb.Append(File.ReadAllText($"{assetsBasePath}\\svg.path\\mail.txt"));
             sb.Append($"</svg></span>{rio.Profile.Email}</div>");
         }
 
         private void GenMobile(StringBuilder sb)
         {
-            sb.Append("<div class=\"mobile\"><span role=\"img\" aria-label=\"phone\" class=\"anticon anticon-phone\"" +
-                "style=\"color: rgb(47, 87, 133); opacity: 0.85;\"><svg viewBox=\"64 64 896 896\" focusable=\"false\"" +
-                "data-icon=\"phone\" width=\"1em\" height=\"1em\" fill=\"currentColor\" aria-hidden=\"true\">");
+            sb.Append("<div class=\"mobile\">");
+            sb.Append("<span role=\"img\" aria-label=\"phone\" class=\"anticon anticon-phone\" style=\"color: rgb(47, 87, 133); opacity: 0.85;\">");
+            sb.Append("<svg viewBox=\"64 64 896 896\" focusable=\"false\" data-icon=\"phone\" width=\"1em\" height=\"1em\" fill=\"currentColor\" aria-hidden=\"true\">");
             sb.Append(File.ReadAllText($"{assetsBasePath}\\svg.path\\mobile.txt"));
             sb.Append($"</svg></span>{rio.Profile.Mobile}</div>");
         }
