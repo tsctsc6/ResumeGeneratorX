c06e2a9910aa25ddb81d9f9c8a6142af6a3e62de
tsctsc6@126.com <tsctsc6@126.com>
2024-09-08 15:37:35 +0800

    完成简要信息和头像的生成

--- ResumeGeneratorX/AssetsPath.cs
+++ ResumeGeneratorX/AssetsPath.cs
@@ -1,6 +1,8 @@
-﻿namespace ResumeGeneratorX;
+﻿using System.Text.RegularExpressions;
 
-public class AssetsPath
+namespace ResumeGeneratorX;
+
+public partial class AssetsPath
 {
     public static string Assets => Path.Combine(AppContext.BaseDirectory, "Assets");
     public static string SvgPath => Path.Combine(Assets, "svg.path");
@@ -8,4 +10,22 @@ public class AssetsPath
     {
         return File.ReadAllText(Path.Combine(SvgPath, $"{name}.txt"));
     }
+
+    public static string GetImgSize(string sizeString)
+    {
+        var sizeStrings = sizeString.Split('x');
+        double w = double.Parse(sizeStrings[0]);
+        double h = double.Parse(sizeStrings[1]);
+        return (84 * h / w).ToString();
+    }
+
+    public static string ImgToBase64(string filePath)
+    {
+        var extension = Path.GetExtension(filePath)[1..];
+        var avatarBase64 = Convert.ToBase64String(File.ReadAllBytes(filePath));
+        return $"data:image/{extension};base64,{avatarBase64}";
+    }
+
+    [GeneratedRegex("height: (\\d+)px")]
+    private static partial Regex FindHeight();
 }
