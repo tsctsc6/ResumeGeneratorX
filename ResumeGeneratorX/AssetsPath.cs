72c8f393e9a9f8d19a915c49ae1ce0ea7f643814
tsctsc6@126.com <tsctsc6@126.com>
2024-09-08 14:49:04 +0800

    完成手机号的生成

---
+++ ResumeGeneratorX/AssetsPath.cs
@@ -0,0 +1,8 @@
+﻿namespace ResumeGeneratorX;
+
+public class AssetsPath
+{
+    public static string Assets => Path.Combine(AppContext.BaseDirectory, "Assets");
+    public static string SvgPath => Path.Combine(Assets, "svg.path");
+    public static string MobileSvgPath = File.ReadAllText(Path.Combine(SvgPath, "mobile.txt"));
+}
