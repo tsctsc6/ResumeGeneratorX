c124bb9f777393f6565a1a14e6322d50d76a44d2
tsctsc6@126.com <tsctsc6@126.com>
2024-09-08 14:56:07 +0800

    添加方法

--- ResumeGeneratorX/AssetsPath.cs
+++ ResumeGeneratorX/AssetsPath.cs
@@ -4,5 +4,8 @@ public class AssetsPath
 {
     public static string Assets => Path.Combine(AppContext.BaseDirectory, "Assets");
     public static string SvgPath => Path.Combine(Assets, "svg.path");
-    public static string MobileSvgPath = File.ReadAllText(Path.Combine(SvgPath, "mobile.txt"));
+    public static string GetSvgPath(string name)
+    {
+        return File.ReadAllText(Path.Combine(SvgPath, $"{name}.txt"));
+    }
 }
